f80ab465a81d3cbde173bc43afef4782cd3a3842
zamgi <denisov72@mail.ru>
2017-01-20 13:54:27 +0300

    Add files via upload

---
+++ --clear-after-build--.cmd
@@ -0,0 +1,7 @@
+del "*.suo" /Q 2> nul
+del "Reference.DiagnosisCodes.web.demo\*.pdb" /Q 2> nul
+rd "Reference.DiagnosisCodes.web.demo\obj" /S/Q 2> nul
+rd "SuffixArray\bin" /S/Q 2> nul
+rd "SuffixArray\obj" /S/Q 2> nul
+rd "Reference.DiagnosisCodes.ConsoleApp\bin" /S/Q 2> nul
+rd "Reference.DiagnosisCodes.ConsoleApp\obj" /S/Q 2> nul
