7e8a99add622c09f8f7179572488839136b223cd
zamgi <denisov72@mail.ru>
2017-01-20 14:10:51 +0300

    Update --clear-after-build--.cmd

--- --clear-after-build--.cmd
+++ --clear-after-build--.cmd
@@ -1,7 +1,7 @@
-del "*.suo" /Q 2> nul
-del "Reference.DiagnosisCodes.web.demo\*.pdb" /Q 2> nul
-rd "Reference.DiagnosisCodes.web.demo\obj" /S/Q 2> nul
-rd "SuffixArray\bin" /S/Q 2> nul
-rd "SuffixArray\obj" /S/Q 2> nul
-rd "Reference.DiagnosisCodes.ConsoleApp\bin" /S/Q 2> nul
-rd "Reference.DiagnosisCodes.ConsoleApp\obj" /S/Q 2> nul
+del "*.suo" /Q
+del "Reference.DiagnosisCodes.web.demo\*.pdb" /Q
+rd "Reference.DiagnosisCodes.web.demo\obj" /S/Q
+rd "SuffixArray\bin" /S/Q
+rd "SuffixArray\obj" /S/Q
+rd "Reference.DiagnosisCodes.ConsoleApp\bin" /S/Q
+rd "Reference.DiagnosisCodes.ConsoleApp\obj" /S/Q
