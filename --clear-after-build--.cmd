558a25834d124ed00cb210ff62f98513b20059aa
zamgi <denisov72@mail.ru>
2020-11-26 23:17:50 +0300

    Update --clear-after-build--.cmd

--- --clear-after-build--.cmd
+++ --clear-after-build--.cmd
@@ -1,6 +1,6 @@
 del "*.suo" /S/Q/F/A
 del "*.csproj.user" /S/Q/F/A
-del "Reference.DiagnosisCodes.web.demo\bin\*.pdb" /Q
+rd "Reference.DiagnosisCodes.web.demo\bin" /S/Q
 rd "Reference.DiagnosisCodes.web.demo\obj" /S/Q
 rd "SuffixArray\bin" /S/Q
 rd "SuffixArray\obj" /S/Q
